048af9b4361a1492b6d1ccb72ff227b5e70a2b3b
Huang Zixun <13051190353@163.com>
2018-10-08 16:11:06 +0200

    perlin noise

--- sketch.js
+++ sketch.js
@@ -1,11 +1,25 @@
-function preload(){
-  // put preload code here
-}
-
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth,windowHeight);
+  frameRate(30);
 }
 
 function draw() {
-  // put drawing code here
+  background(180);
+  for(x=0; x<width; x+=5){
+  	for(y=0; y<width; y+=5){
+    	var n=noise(0.02*x,0.04*y)*255;
+      noStroke();
+      var col=color(n+50,n-100,n-250);
+      fill(col);
+
+      //var size=noise(0.02*x,0.04*y)*3.2
+      ellipse(x,y,3.2);
+      x=x+frameCount/1400;
+      y=y+frameCount/1400;
+
+      if(x>windowWidth+1.1){
+      	frameCount=-frameCount;
+      }
+    }
+  }
 }
